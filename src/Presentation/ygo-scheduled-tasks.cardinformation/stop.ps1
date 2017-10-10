3abdda1ecc8c4a9d44c10bc5c5a688deab65bce5
fablecode <fablecode@users.noreply.github.com>
2017-10-10 18:18:42 +0100

    stop.ps1 updated

--- src/Presentation/ygo-scheduled-tasks.cardinformation/stop.ps1
+++ src/Presentation/ygo-scheduled-tasks.cardinformation/stop.ps1
@@ -1,2 +1,20 @@
-﻿ygo-scheduled-tasks.cardinformation.exe stop
-"ygo-scheduled-tasks.cardinformation is stopped" | Write-Host
+﻿$serviceName = 'YgoCardInformation'
+
+If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+
+    If ((Get-Service $serviceName).Status -eq 'Running') {
+
+        Stop-Service $serviceName
+        Write-Host "Stopping $serviceName"
+
+    } Else {
+
+        Write-Host "$serviceName found, but it is not running."
+
+    }
+
+} Else {
+
+    Write-Host "$serviceName not found"
+
+}
