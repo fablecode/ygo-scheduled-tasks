de4a466c27f9efded2291280f7d1f1075734931f
fablecode <fablecode@users.noreply.github.com>
2018-03-01 23:04:58 +0000

    ContentResultHelpersTests added.

---
+++ tests/unit/ygo-scheduled-tasks.domain.unit.tests/ContentResultHelpersTests.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using wikia.Models.Article.Simple;
+using ygo_scheduled_tasks.domain.Helpers;
+
+namespace ygo_scheduled_tasks.domain.unit.tests
+{
+    [TestFixture]
+    public class ContentResultHelpersTests
+    {
+        [TestCaseSource(nameof(_getSectionContentListCases))]
+        public void Given_A_Section_With_ContentList_Should_Return_All_ListElements(Section section, string[] expected)
+        {
+            // Assert
+
+            // Act
+            var result = ContentResultHelpers.GetSectionContentList(section);
+
+            // Assert
+            result.ShouldBeEquivalentTo(expected);
+        }
+
+        [TestCaseSource(nameof(_getContentListCases))]
+        public void Given_A_Section_With_ContentList_Should_Use_Recursion_To_Traverse_Data(Section section, string[] expected)
+        {
+            // Assert
+
+            // Act
+            var result = ContentResultHelpers.GetSectionContentList(section);
+
+            // Assert
+            result.ShouldBeEquivalentTo(expected);
+        }
+
+
+        static object[] _getSectionContentListCases =
+        {
+            new object[] {new Section
+            {
+                Content = new[]
+                {
+                    new SectionContent
+                    {
+                        Elements = new[]
+                        {
+                            new ListElement
+                            {
+                                Text = "Ancient Fairy Dragon"
+                            },
+                            new ListElement
+                            {
+                                Text = "Blaster, Dragon Ruler of Infernos"
+                            },
+                            new ListElement
+                            {
+                                Text = "Cyber Jar"
+                            }
+                        }
+                    }
+                }
+            }, new [] {"Cyber Jar", "Ancient Fairy Dragon", "Blaster, Dragon Ruler of Infernos"}}
+        };
+
+        static object[] _getContentListCases =
+        {
+            new object[] {new Section
+            {
+                Content = new[]
+                {
+                    new SectionContent
+                    {
+                        Elements = new[]
+                        {
+                            new ListElement
+                            {
+                                Text = "Ancient Fairy Dragon"
+                            },
+                            new ListElement
+                            {
+                                Text = "Blaster, Dragon Ruler of Infernos",
+                                Elements = new[]
+                                {
+                                    new ListElement
+                                    {
+                                        Text = "Master Peace, the True Dracoslaying King",
+                                        Elements = new[]
+                                        {
+                                            new ListElement
+                                            {
+                                                Text = "Mind Master"
+                                            } 
+                                        }
+                                    } 
+                                }
+                            },
+                            new ListElement
+                            {
+                                Text = "Cyber Jar"
+                            }
+                        }
+                    }
+                }
+            }, new [] {"Cyber Jar", "Ancient Fairy Dragon", "Blaster, Dragon Ruler of Infernos", "Master Peace, the True Dracoslaying King", "Mind Master"}}
+        };
+
+    }
+}
