46d8fd3300bbd52908d66e933e25650c25a6a1d5
fablecode <fablecode@users.noreply.github.com>
2018-05-28 13:38:56 +0100

    Scheduled task for card tips added.

---
+++ src/Presentation/ygo-scheduled-tasks.tips/CardTipsJob.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Quartz;
+
+namespace ygo_scheduled_tasks.tips
+{
+    public class CardTipsJob : IJob
+    {
+        private readonly IMediator _mediator;
+
+        public CardTipsJob(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async void Execute(IJobExecutionContext context)
+        {
+            const int pageSize = 500;
+            const string category = "Card Tips";
+
+            await _mediator.Send(new CardTipsTask { Category = category, PageSize = pageSize });
+        }
+    }
+
+    public class CardTipsTask : IRequest
+    {
+        public string Category { get; set; }
+        public int PageSize { get; set; }
+    }
+}
---
+++ src/Presentation/ygo-scheduled-tasks.tips/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using Quartz;
+using Topshelf;
+using Topshelf.Quartz.StructureMap;
+using Topshelf.StructureMap;
+using ygo_scheduled_tasks.cardinformation;
+
+namespace ygo_scheduled_tasks.tips
+{
+    class Program
+    {
+        private static readonly string CronExpression = ConfigurationManager.AppSettings["CronExpression"]; // Every Day at 4:00am
+
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                var container = Ioc.Initialize();
+                x.UseStructureMap(container);
+                x.UseNLog();
+
+                x.Service<CardTipsService>(s =>
+                {
+                    s.ConstructUsingStructureMap();
+
+                    s.BeforeStartingService(_ => Console.WriteLine("Before Start"));
+                    s.BeforeStoppingService(_ => Console.WriteLine("Before Stop"));
+
+                    s.WhenStarted(service => service.OnStart());
+                    s.WhenStopped(service => service.OnStop());
+
+                    s.UseQuartzStructureMap();
+                    
+                    s.ScheduleQuartzJob(q => 
+                        q.WithJob(() => 
+                            JobBuilder.Create<CardTipsJob>().Build())
+                            .AddTrigger(() => TriggerBuilder.Create()
+                                .WithCronSchedule(CronExpression)
+                                .StartNow()
+                                .Build()));
+                });
+
+                x.RunAsLocalSystem()
+                    .DependsOnEventLog()
+                    .StartAutomatically()
+                    .EnableServiceRecovery(rc => rc.RestartService(1));
+
+                var ygoCardTips = "YgoCardTips";
+
+                x.SetServiceName(ygoCardTips);
+                x.SetDisplayName(ygoCardTips);
+                x.SetDescription("Amalgamate card tips data, for all Yugioh cards.");
+            });
+        }
+    }
+}
